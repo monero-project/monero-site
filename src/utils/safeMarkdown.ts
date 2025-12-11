b2dc0814811b205220a77089774ffb9e15e30ebe
redsh4de <25299353+redsh4de@users.noreply.github.com>
2025-12-11 22:37:07 +0200

    feat(exchanges): add exchange page (#83)
    * add exchange page prototype
    
    * layout improvements + mobile
    
    * style updates
    
    * style updates
    
    * simplifications
    
    * style updates
    
    * visual tweaks, torrent button
    
    * move exchange data to json
    
    * localization
    
    * lint fix

--- src/utils/safeMarkdown.ts
+++ src/utils/safeMarkdown.ts
@@ -1,12 +1,31 @@
 import DOMPurify from "isomorphic-dompurify";
 import { marked } from "marked";
 
+const purifyConfig = {
+  ALLOWED_ATTR: [
+    "href",
+    "target",
+    "rel",
+    "class",
+    "id",
+    "title",
+    "alt",
+    "src",
+    "width",
+    "height",
+    "style",
+  ],
+};
+
 export const parse = (markdown: string): string => {
-  return DOMPurify.sanitize(marked.parse(markdown) as string);
+  return DOMPurify.sanitize(marked.parse(markdown) as string, purifyConfig);
 };
 
 export const parseInline = (markdown: string): string => {
-  return DOMPurify.sanitize(marked.parseInline(markdown) as string);
+  return DOMPurify.sanitize(
+    marked.parseInline(markdown) as string,
+    purifyConfig,
+  );
 };
 
 export default {
