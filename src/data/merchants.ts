e7d44a081a1c10f47dee1d6bbca7fc07776bee72
redsh4de <25299353+redsh4de@users.noreply.github.com>
2026-01-10 17:48:21 +0200

    chore(data): migrate merchants.json to merchants.ts

---
+++ src/data/merchants.ts
@@ -0,0 +1,40 @@
+export type Merchant = {
+  id: string;
+  name: string;
+  logo: string;
+  link: string;
+  onionLink?: string;
+};
+
+const merchants: Merchant[] = [
+  {
+    id: "cryptwerk",
+    name: "Cryptwerk",
+    logo: "/media/cryptwerk.jpg",
+    link: "https://cryptwerk.com/pay-with/xmr",
+  },
+  {
+    id: "monerica",
+    name: "Monerica",
+    logo: "/media/monerica-logo.png",
+    link: "https://monerica.com",
+    onionLink:
+      "http://67w4f46nfrfigohl4ypgpo7cjnftl57rorha3zmn4xkll5jvmi6mi4qd.onion",
+  },
+  {
+    id: "xmrbazaar",
+    name: "XMRBazaar",
+    logo: "/media/xmrbazaar.jpg",
+    link: "https://xmrbazaar.com",
+  },
+  {
+    id: "kycnotme",
+    name: "KYCNot.me",
+    logo: "/media/kycnotme.jpg",
+    link: "https://kycnot.me/?currency-mode=or&currencies=xmr",
+    onionLink:
+      "http://kycnotmezdiftahfmc34pqbpicxlnx3jbf5p7jypge7gdvduu7i6qjqd.onion/?currency-mode=or&currencies=xmr",
+  },
+];
+
+export default merchants;
