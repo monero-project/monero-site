a111c31f83c05f9937dc80ce24e798e9176adff7
redsh4de <25299353+redsh4de@users.noreply.github.com>
2026-01-12 22:59:12 +0200

    remove ML localizations in preperation for weblate

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,8 +1,5 @@
 export const defaultLocale = "en";
 export const locales = {
   en: "en-US",
-  es: "es-ES",
-  fr: "fr-FR",
-  ar: "ar-SA",
 };
 export const rtlLocales = ["ar", "he", "fa", "ur"]; // Arabic, Hebrew, Persian, Urdu
