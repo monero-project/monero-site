1cb5b27f9b8a9be020e395ece910858a90cee3ea
redsh4de <25299353+redsh4de@users.noreply.github.com>
2026-01-17 15:19:55 +0200

    integrate feedback

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,6 +1,6 @@
 export const defaultLocale = "en";
 export const locales = {
   en: "en-US",
-  ar: "ar",
+  ar: "ar-SA",
 };
 export const rtlLocales = ["ar", "he", "fa", "ur"]; // Arabic, Hebrew, Persian, Urdu
